eadfbb91812161ae25def1328da13d2721100010
Eugenia Petrunia <avepetrunia@gmail.com>
2020-06-19 09:00:19 +0300

    Create checkMultiplicity() function
    Receive two numbers. Check them for multiplicity.

--- index.js
+++ index.js
@@ -8,3 +8,15 @@
 function isAdult(age) {
     return age >= 18;
 }
+
+// Task 2
+
+/**
+ *
+ * @param {number} num1 - First number
+ * @param {number} num2 - Second number
+ * @returns {boolean} - Check multiplicity of first and second numbers
+ */
+function checkMultiplicity(num1, num2) {
+    return num1 % num2 === 0;
+}
